32219c5f0cfc7a632faac0e90d6a8ddf341d58f3
iamthepoe <lucagamerbrime@gmail.com>
2022-06-24 22:52:55 +0900

    settando as configurações e componentes do script

--- www/components/script.js
+++ www/components/script.js
@@ -1,17 +1,12 @@
+const selectOne = document.querySelector('#selectOne');
+const selectTwo = document.querySelector('#selectTwo');
+
 const options = {
     method: 'GET',
     mode: 'cors',
     catch: 'default'
 }
-function getPrice(coinA, coinB){
-    fetch(`https://economia.awesomeapi.com.br/json/last/${coinA}-${coinB}`, options)
-        .then((res)=>{
-            res.json()
-        .then((res)=>{
-            return res`.${coinA}${coinB}`;
-        });
-    }).catch(error=>console.log(error));
-}
 
-alert(getPrice('USD', 'BRL'));
+
+
         
