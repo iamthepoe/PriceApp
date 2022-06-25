041c2ebb29aec5f36394dd58bd82c17db6809851
iamthepoe <lucagamerbrime@gmail.com>
2022-06-25 23:27:32 +0900

    adicionando moedas e atualizando script

--- www/components/script.js
+++ www/components/script.js
@@ -45,12 +45,17 @@ function writeInfo(info){
     `
 }
 
-
-select.forEach(item=>{
-    item.addEventListener('change', ()=>{
-        getInfo(select[0].value, select[1].value)
-        .then(info=>{
-            writeInfo(info);
-        })
+getInfo('BRL', 'USD')
+    .then(info=>{
+        writeInfo(info);
     });
-});
+
+select[0].addEventListener('change', ()=>{
+    getInfo(select[0].value, select[1].value)
+        .then(info=>writeInfo(info));
+})
+
+select[1].addEventListener('change', ()=>{
+    getInfo(select[0].value, select[1].value)
+        .then(info=>writeInfo(info));
+})
